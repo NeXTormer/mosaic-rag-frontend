2ffbb9e96e09c79df739ad8d549e49a1cc25a97e
Felix Holz <felix.holz@me.com>
2025-01-13 22:48:28 +0100

    improve search result ui

--- lib/backend/search_manager.dart
+++ lib/backend/search_manager.dart
@@ -8,6 +8,10 @@ class SearchManager extends ChangeNotifier {
   String query = '';
 
   ResultList resultList = ResultList([]);
+  List<String> resultColumns = <String>[];
+  List<String> chipColumns = <String>[];
+  Map<String, List<int>> resultColumnsWordCountList = Map<String, List<int>>();
+  Map<String, int> resultColumnsWordCount = Map<String, int>();
 
   bool showLoadingBar = false;
 
@@ -54,6 +58,66 @@ class SearchManager extends ChangeNotifier {
     }
 
     resultList = await MosaicRS.runPipeline(parameters);
+
+    final stopwatch = Stopwatch();
+
+    stopwatch.start();
+
+    resultColumns.clear();
+    resultColumnsWordCount.clear();
+    chipColumns.clear();
+
+    for (final result in resultList.data) {
+      for (final key in result.keys) {
+        if (!resultColumns.contains(key)) {
+          resultColumns.add(key);
+        }
+      }
+    }
+
+    // only get columns that exist in every row
+    for (final result in resultList.data) {
+      for (final column in resultColumns) {
+        if (!result.containsKey(column)) {
+          resultColumns.remove(column);
+        }
+      }
+    }
+
+    for (final column in resultColumns) {
+      resultColumnsWordCountList[column] = <int>[];
+    }
+
+    for (final result in resultList.data) {
+      for (final column in resultColumns) {
+        if (result[column] is String) {
+          resultColumnsWordCountList[column]!.add(result[column].length);
+        } else {
+          resultColumnsWordCountList[column]!.add('${result[column]}'.length);
+        }
+      }
+    }
+
+    for (final column in resultColumns) {
+      resultColumnsWordCountList[column]!.sort();
+      resultColumnsWordCount[column] = resultColumnsWordCountList[column]![
+          resultColumnsWordCountList[column]!.length ~/ 2];
+    }
+
+    var sortedEntries = resultColumnsWordCount.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value));
+    final x = Map.fromEntries(sortedEntries);
+
+    resultColumns = x.keys.toList();
+
+    for (final column in resultColumns) {
+      if (x[column]! < 6) {
+        chipColumns.add(column);
+      }
+    }
+
+    stopwatch.stop();
+    print('Result postprocessing time: ${stopwatch.elapsedMicroseconds} us');
 
     showLoadingBar = false;
     notifyListeners();
---
+++ lib/widgets/chip_selector.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mosaic_rs_application/main.dart';
+
+class ChipSelector extends StatelessWidget {
+  const ChipSelector(
+      {super.key,
+      this.height = 44,
+      this.borderRadius = 12,
+      required this.onTap,
+      required this.child,
+      required this.selected});
+
+  final double height;
+  final double borderRadius;
+  final Function() onTap;
+  final Widget child;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(borderRadius),
+          color: theme.cardBackgroundColor,
+          border: Border.all(
+              color: selected ? theme.mainColor : theme.cardBorderColor,
+              width: 1)),
+      child: Container(
+          height: height,
+          child: Material(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.transparent,
+              child: InkWell(
+                  customBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  splashColor: Colors.grey.withAlpha(32),
+                  highlightColor: Colors.grey.withAlpha(15),
+                  onTap: onTap,
+                  child: child))),
+    );
+  }
+}
